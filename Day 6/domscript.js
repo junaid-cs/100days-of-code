c2be5085449df86d638abec477c5a5746e1b16c4
Jay <85805386+Jay-khan421@users.noreply.github.com>
2022-11-18 10:19:42 +0500

    direct selecting dom using document

--- Day 6/domscript.js
+++ Day 6/domscript.js
@@ -1,6 +1,19 @@
 let a=document.body.firstElementChild;
-console.log(a);
-console.log(document.body.lastChild);
+// console.log(a);
+// console.log(document.body.lastChild);
 let b=document.body.childNodes;
 b=Array.from(b);
-console.log(b);
+// console.log(b);
+
+let val;
+val = document;
+val =document.all;
+val = document.all[2];
+val = document.URL;
+val = document.domain;
+val = document.characterSet;
+val = document.head;
+val = document.body;
+val = document.links;
+val = document.scripts;
+console.log(val);

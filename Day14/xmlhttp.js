d1c77822a9538bb9d347ec7d1e780f071b56b917
Jay <85805386+Jay-khan421@users.noreply.github.com>
2022-12-14 15:54:45 +0500

    api single product

---
+++ Day14/xmlhttp.js
@@ -0,0 +1,51 @@
+let single_btn=document.querySelector('.single-btn');
+let single_output=document.querySelector('.single-output');
+let multiple_btn=document.querySelector('.multiple-btn');
+let multiple_output=document.querySelector('.multiple-output');
+let input = document.querySelector('[data-input]');
+
+single_btn.addEventListener('click',loadsingledata)
+function loadsingledata(){
+    console.log('enter')
+    let xml=new XMLHttpRequest;
+    xml.open('GET','https://dummyjson.com/products/3',true);
+    xml.onprogress = function(){
+        single_output.innerHTML = '<h1>Loading</h1>';
+        console.log('loading')
+    }
+    xml.onload = function(){
+        if(this.status == 200){
+        let response =JSON.parse( this.responseText);
+        single_output.innerHTML = ` 
+        <li>${response.id}</li>
+        <li>${response.brand}</li>
+        <li>${response.category}</li>
+        <li>${response.discountPercentage}</li>
+        <li>${response.description
+        }</li>
+        `
+        // console.log(response);
+    }}
+    xml.send();
+}
+// loadsingledata();
+
+multiple_btn.addEventListener('click',loadmultipledata);
+function loadmultipledata(){
+    let xhr = new XMLHttpRequest;
+    xhr.open('GET',`https://dummyjson.com/products/${input.value}`,true);
+    xhr.onload = function ()
+    {
+        if(this.status == 200){
+            let mul_response=JSON.parse(this.responseText);
+            let output = '';
+
+            // mul_response.forEach(response => {
+            //     console.log(response);
+            // });
+            console.log(mul_response, input.value);
+        }
+    }
+    xhr.send();
+}
+loadmultipledata();

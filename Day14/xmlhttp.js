9afe1353ceaec2db1a647d4c3f330a49ba848004
Jay <85805386+Jay-khan421@users.noreply.github.com>
2022-12-15 11:13:43 +0500

    Update xmlhttp.js

--- Day14/xmlhttp.js
+++ Day14/xmlhttp.js
@@ -2,8 +2,8 @@ let single_btn=document.querySelector('.single-btn');
 let single_output=document.querySelector('.single-output');
 let multiple_btn=document.querySelector('.multiple-btn');
 let multiple_output=document.querySelector('.multiple-output');
-let input = document.querySelector('[data-input]');
-console.log(input);
+
+// console.log(input);
 
 single_btn.addEventListener('click',loadsingledata)
 function loadsingledata(){
@@ -33,18 +33,23 @@ function loadsingledata(){
 
 multiple_btn.addEventListener('click',loadmultipledata);
 function loadmultipledata(e){
-    console.log(input.value)
+   
     let xhr = new XMLHttpRequest;
     xhr.open('GET',`https://dummyjson.com/products`,true);
     xhr.onload = function ()
     {
         if(this.status == 200){
+            let input = document.querySelector('[data-input]');
+            console.log(input.value)
+            input =parseInt( input.value);
             let mul_response=JSON.parse(this.responseText);
             let mu_response=JSON.stringify(this.responseText);
             let output = '';
-            console.log(mu_response[2], input.value);
-            mul_response.products.forEach(function(response) {
-                console.log(JSON.stringify(response));
+            data = mul_response.products;
+            loop = data.slice(0,input);
+            console.log( typeof input);
+            loop.forEach(function(response) {
+                // console.log(response);
                 output +=
                 `<li> ${response.id}</li>
                 <li> ${response.title}</li>

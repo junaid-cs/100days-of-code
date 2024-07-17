050997c4d76fc8e85eb496f085d5d42bab156599
Abdul wasi <abdulwasi0532@gmail.com>
2024-07-17 15:17:48 +0500

    This is my react hooks practice(useEffect, useReducer and useState) are used.

---
+++ Abdulwasi/react-hooks/src/App.js
@@ -0,0 +1,11 @@
+
+import React from "react"; 
+// import UseState from "./component/Hooks/useState";
+// import UseEffect from "./component/Hooks/useEffect";
+import UseReducer from "./component/Hooks/useReducer";
+
+const App = () => {
+  return <UseReducer />;
+};
+
+export default App;

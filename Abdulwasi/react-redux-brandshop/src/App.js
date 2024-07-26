d727705b1928e5f3cbf54cb540ef52987b3da146
Abdul wasi <abdulwasi0532@gmail.com>
2024-07-26 22:11:09 +0500

    React-redux-brandshop
    I made this react-redux-brandshop. Which is my practice and I used semantic ui and Fake store api in it.

---
+++ Abdulwasi/react-redux-brandshop/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProductListing from "./containers/ProductListing";
+import Header from "./containers/Header";
+import "./App.css";
+import ProductDetails from "./containers/ProductDetails";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<ProductListing />} />
+          <Route path="/product/:productId" element={<ProductDetails />} />
+          <Route>404 Not Found!</Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
